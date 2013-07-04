d79b4fa204e8129c9cfb08a634852d56b17c29b1
pixel-industry <support@pixel-industry.com>
2013-07-04 16:22:16 +0200

    Initial commit

---
+++ twitter/tweets.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once("twitteroauth.php"); //Path to twitteroauth library
+ 
+$twitteruser = $_GET['username'];
+$notweets = $_GET['limit'];
+$consumerkey = "";
+$consumersecret = "";
+$accesstoken = "";
+$accesstokensecret = "";
+ 
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+  
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+ 
+$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+ 
+echo json_encode($tweets);
+?>
