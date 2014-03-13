b4f6831fa2617ac721a49d499a202bf602b55aa5
Vladimir Mrkela <mrkyvk@gmail.com>
2014-03-13 18:42:01 +0100

    Remove Twitter keys.

--- source/twitter/tweets.php
+++ source/twitter/tweets.php
@@ -4,10 +4,10 @@ require_once("twitteroauth.php"); //Path to twitteroauth library
  
 $twitteruser = $_GET['username'];
 $notweets = $_GET['limit'];
-$consumerkey = "8NQiQlbkGAkN8mNY2ieg";
-$consumersecret = "sgxVB6NF1mWa7xkpPMWyEwGEJBWn4j5HmSL0gc0s";
-$accesstoken = "766190528-2hKqgFrBA1fPm168gr0o8Fjc91DOurnH7Ok4XSIR";
-$accesstokensecret = "zIjansn5IBYStdb7uGWuQMm9yXhFJAHbteIdVdxgd0";
+$consumerkey = "";
+$consumersecret = "";
+$accesstoken = "";
+$accesstokensecret = "";
  
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
